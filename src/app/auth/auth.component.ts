dee7c51735a79fc06d4e3b920da78565fdf19a43
devsuzarte <ben.dantassuzarte@gmail.com>
2025-04-09 15:32:55 -0300

    feat: create auth html

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LoginFormComponent } from '../components/login-form/login-form.component';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [LoginFormComponent],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.scss'
 })
