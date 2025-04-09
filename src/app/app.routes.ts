358f2d4509982719e14187ee4d815a6b037de7a6
devsuzarte <ben.dantassuzarte@gmail.com>
2025-04-09 15:31:54 -0300

    feat: create login form component and /auth route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    pathMatch: 'full',
+    loadComponent: () => {
+      return import("./auth/auth.component").then((m) => m.AuthComponent)
+    }
+  }
+];
