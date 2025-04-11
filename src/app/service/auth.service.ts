07ba4928d955036c9b5550c0372d17ad82b21b13
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-04-11 20:46:14 -0300

    feat: create auth request on service

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  private http = inject(HttpClient)
+  private url = "http://localhost:3000"
+  
+  auth(username: string, password: string) {
+    return this.http.post(`${this.url}/login`, { nome: username, senha: password })
+  }
 }
