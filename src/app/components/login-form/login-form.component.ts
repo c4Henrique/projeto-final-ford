51331cfd8ef7cd3607d94cccba1ecfd24bcc1abd
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-04-11 21:12:05 -0300

    feat: add service basic implementation in login component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../service/auth.service';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
+  private authService = inject(AuthService)
 
+  loginForm = new FormGroup({
+    username: new FormControl(""),
+    password: new FormControl("")
+  })
+
+  authorized = signal(false)
+  errorMessage = signal("")
+
+  login() {
+    const { username, password } = this.loginForm.value
+
+    if(!this.loginForm.valid || !username || !password) {
+      this.errorMessage = signal("Existem campos não preenchidos!")
+      return
+    }
+
+    this.authService.auth(username, password)
+  }
 }
