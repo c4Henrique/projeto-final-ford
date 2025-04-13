403cbaa3b689d002ce21cb1614f932819bf13860
BenSuzarte <ben.dantassuzarte@gmail.com>
2025-04-13 10:27:15 -0300

    feat: create welcome popup

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from "../components/menu/menu.component";
+import { WelcomeComponent } from '../components/welcome/welcome.component';
 
 @Component({
   selector: 'app-home',
-  imports: [MenuComponent],
+  imports: [MenuComponent, WelcomeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
