4ec5795b196044a31dba01133cf063baddb2e337
devsuzarte <ben.dantassuzarte@gmail.com>
2025-05-07 10:18:53 -0300

    create interactive vin input

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from '../components/card/card.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CardComponent],
+  imports: [CardComponent, FormsModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+  vin = "1NXBR12E11Z543327"
+  inputVin = ""
+  isEditingVin = signal(false)
 
+  editVin() {
+    this.isEditingVin.set(!this.isEditingVin());
+  }
+
+  showVinInfos() {
+    this.vin = this.inputVin
+    this.editVin()
+
+    this.inputVin = ""
+  }
 }
