08c8a034384c6fb397dc2ad924da916a5292fe1a
devsuzarte <ben.dantassuzarte@gmail.com>
2025-05-07 08:55:05 -0300

    change card to add input title and customize dashboard table

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -8,7 +8,7 @@ import { Observable, tap } from 'rxjs';
 })
 export class AuthService {
   private http = inject(HttpClient)
-  private url = "http://localhost:3000"
+  private url = "http://localhost:3001"
   
   auth(username: string, password: string): Observable<User> {
     return this.http.post<User>(`${this.url}/login`, { nome: username, senha: password })
